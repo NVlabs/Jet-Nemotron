4ce3a7c8ec58de767bf16ced2c0bc85784de8130
Yee Man Chan <ymchan@gmail.com>
2025-11-20 20:24:47 +0800

    compatibility with transformers 4.57.1

--- jetai/modeling/hf/modeling_jet_nemotron.py
+++ jetai/modeling/hf/modeling_jet_nemotron.py
@@ -16,8 +16,8 @@
 
 # This file is modified from https://github.com/huggingface/transformers/blob/main/src/transformers/models/qwen2/modeling_qwen2.py
 
-from functools import partial
-from typing import Callable, Optional, Tuple, Union
+from functools import partial, wraps
+from typing import Callable, Optional, Tuple, Union, Any, TypedDict
 
 import torch
 from torch import nn
@@ -34,7 +34,6 @@ from transformers.modeling_rope_utils import ROPE_INIT_FUNCTIONS, dynamic_rope_u
 from transformers.modeling_utils import PreTrainedModel, ALL_ATTENTION_FUNCTIONS
 from transformers.processing_utils import Unpack
 from transformers.utils import (
-    LossKwargs,
     add_start_docstrings,
     add_start_docstrings_to_model_forward,
     can_return_tuple,
@@ -63,6 +62,109 @@ logger = logging.get_logger(__name__)
 _CHECKPOINT_FOR_DOC = "jet-ai/Jet-Nemotron-2B"
 _CONFIG_FOR_DOC = "JetNemotronConfig"
 
+class LossKwargs(TypedDict, total=False):
+    """
+    Keyword arguments to be passed to the loss function
+
+    Attributes:
+        num_items_in_batch (`Optional[torch.Tensor]`, *optional*):
+            Number of items in the batch. It is recommended to pass it when
+            you are doing gradient accumulation.
+    """
+
+    num_items_in_batch: Optional["torch.Tensor"]
+
+            
+def is_timm_config_dict(config_dict: dict[str, Any]) -> bool:
+    """Checks whether a config dict is a timm config dict."""
+    return "pretrained_cfg" in config_dict
+
+    
+def is_timm_local_checkpoint(pretrained_model_path: str) -> bool:
+    """
+    Checks whether a checkpoint is a timm model checkpoint.
+    """
+    if pretrained_model_path is None:
+        return False
+
+    # in case it's Path, not str
+    pretrained_model_path = str(pretrained_model_path)
+
+    is_file = os.path.isfile(pretrained_model_path)
+    is_dir = os.path.isdir(pretrained_model_path)
+
+    # pretrained_model_path is a file
+    if is_file and pretrained_model_path.endswith(".json"):
+        with open(pretrained_model_path) as f:
+            config_dict = json.load(f)
+        return is_timm_config_dict(config_dict)
+
+    # pretrained_model_path is a directory with a config.json
+    if is_dir and os.path.exists(os.path.join(pretrained_model_path, "config.json")):
+        with open(os.path.join(pretrained_model_path, "config.json")) as f:
+            config_dict = json.load(f)
+        return is_timm_config_dict(config_dict)
+
+    return False
+
+
+def set_attribute_for_modules(module: "torch.nn.Module", key: str, value: Any):
+    """
+    Set a value to a module and all submodules.
+    """
+    setattr(module, key, value)
+    for submodule in module.children():
+        set_attribute_for_modules(submodule, key, value)
+
+
+def del_attribute_from_modules(module: "torch.nn.Module", key: str):
+    """
+    Delete a value from a module and all submodules.
+    """
+    # because we might remove it previously in case it's a shared module, e.g. activation function
+    if hasattr(module, key):
+        delattr(module, key)
+
+    for submodule in module.children():
+        del_attribute_from_modules(submodule, key)
+
+
+def can_return_tuple(func):
+    """
+    Decorator to wrap model method, to call output.to_tuple() if return_dict=False passed as a kwarg or
+    use_return_dict=False is set in the config.
+
+    Note:
+        output.to_tuple() convert output to tuple skipping all `None` values.
+    """
+
+    @wraps(func)
+    def wrapper(self, *args, **kwargs):
+        is_requested_to_return_tuple = kwargs.pop("return_dict", True) is False
+        is_configured_to_return_tuple = self.config.use_return_dict is False if hasattr(self, "config") else False
+
+        # The following allows to convert output to tuple ONLY on top level forward call,
+        # while internal modules of the model will return Output objects
+        # to be able to use name-based attribute access in modeling code.
+
+        # We will check if we are on top level module, if so, turn off to tuple conversion for all
+        # underling calls.
+        is_top_level_module = getattr(self, "_is_top_level_module", True)
+        if is_configured_to_return_tuple and is_top_level_module:
+            set_attribute_for_modules(self, "_is_top_level_module", False)
+
+        try:
+            output = func(self, *args, **kwargs)
+            if is_requested_to_return_tuple or (is_configured_to_return_tuple and is_top_level_module):
+                output = output.to_tuple()
+        finally:
+            # Remove the flag after the model forward call is finished.
+            if is_configured_to_return_tuple and is_top_level_module:
+                del_attribute_from_modules(self, "_is_top_level_module")
+
+        return output
+
+    return wrapper
 
 class JetNemotronMLP(nn.Module):
     def __init__(self, config: JetNemotronConfig):
@@ -940,6 +1042,7 @@ class JetNemotronForCausalLM(JetNemotronPreTrainedModel, GenerationMixin):
             attentions=outputs.attentions,
         )
 
+    # this version with device argument is for transformers <=4.53
     def _prepare_cache_for_generation(
         self,
         generation_config: GenerationConfig,
@@ -948,6 +1051,19 @@ class JetNemotronForCausalLM(JetNemotronPreTrainedModel, GenerationMixin):
         batch_size: int,
         max_cache_length: int,
         device: torch.device,
+    ) -> bool:
+        assert not generation_config.return_legacy_cache, "Legacy cache is not supported for generation."
+        if generation_config.use_cache is False:
+            return
+        model_kwargs["past_key_values"] = JetNemotronCache()
+    
+    def _prepare_cache_for_generation(
+        self,
+        generation_config: GenerationConfig,
+        model_kwargs: dict,
+        assistant_model: "PreTrainedModel",
+        batch_size: int,
+        max_cache_length: int,
     ) -> bool:
         assert not generation_config.return_legacy_cache, "Legacy cache is not supported for generation."
         if generation_config.use_cache is False:
